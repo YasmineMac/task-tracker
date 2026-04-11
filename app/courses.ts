42f63987cf208b5693fb47e49b2b20ad33f90931
Yasmine MacCallum <yasminemaccallum@Yasmines-MacBook-Pro.local>
2026-04-11 13:49:13 +0200

    update text

--- app/courses.ts
+++ app/courses.ts
@@ -1,10 +1,10 @@
 export type Course = { id: string; label: string };
 
 export const COURSES: Course[] = [
-  { id: "robotics_studio", label: "Robotics Studio" },
+  { id: "robotics_studio", label: "🪾🌱 Robotics Studio" },
   { id: "after_gaudi", label: "After Gaudí" },
-  { id: "fab_ar", label: "Fab.AR" },
-  { id: "computational_design", label: "Computational Design" },
-  { id: "the_yas_project", label: "The Yas Project" },
+  { id: "fab_ar", label: "🧬 Fab.AR" },
+  { id: "computational_design", label: "🪲 Computational Design" },
+  { id: "the_yas_project", label: "❄️ The Yas Project" },
 ];
 
